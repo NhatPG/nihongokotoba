d4e961cd73a9c69eab95785082dc06c7b08c03d5
Phan Gia Nhat <pgnhat@gmail.com>
2019-09-13 17:10:23 +0700

    thêm timmer, file data

--- src/NihongoKotoba/Form1.cs
+++ src/NihongoKotoba/Form1.cs
@@ -12,22 +12,38 @@ namespace NihongoKotoba
 {
     public partial class Form1 : Form
     {
+        private Font TitleFont = new Font(FontFamily.GenericSansSerif, 16, FontStyle.Bold);
+        private Font ContentFont = new Font(FontFamily.GenericSansSerif, 14);
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Interval = 20000;
+            timer1.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Font TitleFont = new Font(FontFamily.GenericSansSerif, 16, FontStyle.Bold);
-            Font ContentFont = new Font(FontFamily.GenericSansSerif, 14);
+            contextMenuStrip1.Items.Add(new ToolStripMenuItem("Đã thuộc", null, null, "Fruit"));
+        }
 
+        private void ToolStripItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
             var popupNotifier = new PopupNotifier();
 
-            popupNotifier.TitleText = "脱ぬぎます [ぬぎます]";
+            popupNotifier.TitleText = "脱ぎます [ぬぎます] - [Nhóm 1]";
             popupNotifier.ContentText = "Cởi bỏ (quần áo, giày,…)\nVí dụ:部屋に入る前に靴を脱ぬぎなさい。\nTrước khi vào phòng hãy cởi giày.";
+            popupNotifier.ShowCloseButton = true;
+            popupNotifier.TitlePadding = new Padding(0, 0, 0, 5);
             popupNotifier.TitleFont = TitleFont;
             popupNotifier.ContentFont = ContentFont;
+            popupNotifier.Delay = 10000;
+            popupNotifier.OptionsMenu = contextMenuStrip1;
+            popupNotifier.ShowOptionsButton = true;
             popupNotifier.Popup();
         }
     }
