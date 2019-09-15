f046123ee597f18e912ea97711508cc31ebe789b
Phan Gia Nhat <nhatpg@outlook.com>
2019-09-15 16:32:59 +0700

    Lấy từ vựng từ file excel

--- src/NihongoKotoba/Form1.Designer.cs
+++ src/NihongoKotoba/Form1.Designer.cs
@@ -39,17 +39,20 @@
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ResumeLayout(false);
 
         }
--- src/NihongoKotoba/Form1.cs
+++ src/NihongoKotoba/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,17 +16,52 @@ namespace NihongoKotoba
     {
         private Font TitleFont = new Font(FontFamily.GenericSansSerif, 16, FontStyle.Bold);
         private Font ContentFont = new Font(FontFamily.GenericSansSerif, 14);
+        private List<Kotoba> items = new List<Kotoba>();
+        private int total = 0;
 
         public Form1()
         {
             InitializeComponent();
-            timer1.Interval = 20000;
+            contextMenuStrip1.Items.Add(new ToolStripMenuItem("Đã thuộc", null, null, "Fruit"));
+            timer1.Interval = 300000;
             timer1.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            contextMenuStrip1.Items.Add(new ToolStripMenuItem("Đã thuộc", null, null, "Fruit"));
+            this.Hide();
+            FileInfo existingFile = new FileInfo("Data.xlsx");
+            using (ExcelPackage package = new ExcelPackage(existingFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook?.Worksheets[1];
+                total = worksheet.Dimension.End.Row;
+
+                for (int row = 1; row <= total; row++)
+                {
+                    items.Add(new Kotoba() {
+                        OriginalKotoba = worksheet.Cells[row, 1]?.Value?.ToString(),
+                        Hiragana = worksheet.Cells[row, 2]?.Value?.ToString(),
+                        Type = worksheet.Cells[row, 3]?.Value?.ToString(),
+                        VietnameseMeaning = worksheet.Cells[row, 4]?.Value?.ToString(),
+                        Example = worksheet.Cells[row, 5]?.Value?.ToString(),
+                        ExampleMeaning = worksheet.Cells[row, 6]?.Value?.ToString()
+                    });;
+                }
+            }
+
+            ShowPopup();
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized
+            //hide it from the task bar
+            //and show the system tray icon (represented by the NotifyIcon control)
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
         }
 
         private void ToolStripItem_Click(object sender, EventArgs e)
@@ -33,18 +70,46 @@ namespace NihongoKotoba
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            var popupNotifier = new PopupNotifier();
-
-            popupNotifier.TitleText = "脱ぎます [ぬぎます] - [Nhóm 1]";
-            popupNotifier.ContentText = "Cởi bỏ (quần áo, giày,…)\nVí dụ:部屋に入る前に靴を脱ぬぎなさい。\nTrước khi vào phòng hãy cởi giày.";
-            popupNotifier.ShowCloseButton = true;
-            popupNotifier.TitlePadding = new Padding(0, 0, 0, 5);
-            popupNotifier.TitleFont = TitleFont;
-            popupNotifier.ContentFont = ContentFont;
-            popupNotifier.Delay = 10000;
-            popupNotifier.OptionsMenu = contextMenuStrip1;
-            popupNotifier.ShowOptionsButton = true;
-            popupNotifier.Popup();
+            ShowPopup();
         }
+
+        private void ShowPopup()
+        {
+            var item = items[RandomNumber(0, total-1)];
+
+            if(item != null)
+            {
+                var popupNotifier = new PopupNotifier();
+                popupNotifier.TitleText = $"{item.Hiragana} - {item.OriginalKotoba} - [{item.Type}]";
+                popupNotifier.ContentText = $"{item.VietnameseMeaning}\n{item.Example}\n{item.ExampleMeaning}";
+                popupNotifier.ShowCloseButton = true;
+                popupNotifier.TitlePadding = new Padding(0, 0, 0, 5);
+                popupNotifier.Size = new Size() { Height = 120, Width = 500 };
+                popupNotifier.Scroll = false;
+                popupNotifier.TitleFont = TitleFont;
+                popupNotifier.ContentFont = ContentFont;
+                popupNotifier.Delay = 20000;
+                popupNotifier.OptionsMenu = contextMenuStrip1;
+                popupNotifier.ShowOptionsButton = true;
+                popupNotifier.Popup();
+                items.Remove(item);
+            }            
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+
+    public class Kotoba
+    {
+        public string OriginalKotoba = string.Empty;
+        public string Hiragana = string.Empty;
+        public string Type = string.Empty;
+        public string VietnameseMeaning = string.Empty;
+        public string Example = string.Empty;
+        public string ExampleMeaning = string.Empty;
     }
 }
